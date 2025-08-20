619a6d248174d0ecee1e5ed4a51ad693c67eea7b
Dhananjaya <144225924+DHANANJAYA0013@users.noreply.github.com>
2025-08-20 20:07:37 +0530

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // just apply the plugin without version here
+}
+
+
+android {
+    namespace = "com.example.servehub"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.servehub"
+        minSdk = 23 // 🔁 Changed from 21 to 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/servehub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.servehub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
